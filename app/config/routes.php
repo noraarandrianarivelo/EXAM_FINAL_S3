eb07f2ccf8ac1b79c79f4fcde8f1b767b289a12c
noraarandrianarivelo <mahatsangyaaron.randrianarivelo@outlook.com>
2026-02-16 23:27:27 +0300

    creation route pour tout simulet et recapituler

--- app/config/routes.php
+++ app/config/routes.php
@@ -18,18 +18,26 @@ $router->group('', function(Router $router) use ($app) {
 
 	$router->get('/', [ DashboardController::class, 'showDashboard' ]);
 
-	// Routes pour les dons avec dispatch automatique
+	// Routes pour la récapitulation
+	$router->get('/recapitulation', [ DashboardController::class, 'recapitulation' ]);
+	$router->get('/api/stats-recapitulation', [ DashboardController::class, 'getStatsRecapitulation' ]);
+
+	// Routes pour les dons (sans dispatch automatique)
 	$router->group('/dons', function(Router $router) {
 		$router->get('/create', [ DonController::class, 'create' ]);
 		$router->post('/store', [ DonController::class, 'store' ]);
-		$router->get('/@id:[0-9]+/dispatch-result', [ DonController::class, 'showDispatchResult' ]);
 	});
 
 	// Routes de test pour le dispatch
 	$router->group('/test', function(Router $router) {
 		$router->get('/dispatch', [ TestController::class, 'index' ]);
 		$router->get('/dispatch/don/@id:[0-9]+', [ TestController::class, 'showDon' ]);
+		$router->get('/dispatch/don/@id:[0-9]+/simuler', [ TestController::class, 'simuler' ]);
 		$router->post('/dispatch/don/@id:[0-9]+', [ TestController::class, 'dispatch' ]);
+		
+		// Routes pour dispatch GÉNÉRAL (tous les dons)
+		$router->get('/dispatch/simuler-tout', [ TestController::class, 'simulerTout' ]);
+		$router->post('/dispatch/valider-tout', [ TestController::class, 'dispatcherTout' ]);
 	});
 
 	// Routes pour les achats
